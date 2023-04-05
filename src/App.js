7440bff79fd8efcddc1f5a17430a0485b24974bb
Oleksandr-Berd <alex.berd86@gmail.com>
2023-04-05 13:15:20 +0300

    auth client

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import "./App.css";
 import { Routes, Route, Navigate } from "react-router-dom";
-import { lazy, Suspense } from "react";
+import { lazy, Suspense, useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+
 import { Dna } from "react-loader-spinner";
-import { useEffect, useState } from "react";
 import { postNewData } from "./Utilities/helpers";
+import { PrivateRoute } from "./PrivateRoute";
+import { RestrictedRoute } from "./RestrictedRoute";
+import { useAuth } from "./Utilities/Hooks/useAuth";
+import { refreshUser } from "../src/Redux/Auth/operations";
+import { UserMenu } from "./Components/UserMenu/UserMenu";
+import { AuthNav } from "./Components/AuthNav/AuthNav";
+
+
+const RegisterPage = lazy(() => import("./Pages/Register"));
+const LoginPage = lazy(() => import("./Pages/Login"));
 const TitleHeader = lazy(() => import("./Components/Header/TitleHead"));
 const Header = lazy(() => import("./Components/Header/Header"));
 const Home = lazy(() => import("./Components/Home/Home"));
@@ -22,11 +33,19 @@ const NewData = lazy(() => import("./Components/NewData/NewData"));
 function App() {
   const [finalBody, setFinalBody] = useState({});
   const [finalPath, setFinalPath] = useState("");
+  const { isLoggedIn } = useAuth();
 
   const handleFormSubmitAddData = (body, locPath) => {
     setFinalBody(body);
     setFinalPath(locPath);
   };
+
+  const dispatch = useDispatch();
+  const { isRefreshing } = useAuth();
+
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
 
   useEffect(() => {
     if (!finalBody || !finalPath) {
@@ -35,7 +54,22 @@ function App() {
     postNewData(finalBody, finalPath);
   }, [finalBody, finalPath]);
 
-  return (
+  return isRefreshing ? (
+    <b>
+      <Suspense
+        fallback={
+          <Dna
+            visible={true}
+            height="80"
+            width="80"
+            ariaLabel="dna-loading"
+            wrapperStyle={{}}
+            wrapperClass="dna-wrapper"
+          />
+        }
+      />
+    </b>
+  ) : (
     <div className="App">
       <Suspense
         fallback={
@@ -51,12 +85,33 @@ function App() {
       >
         <Header>
           <TitleHeader />
+          {isLoggedIn ? <UserMenu /> : <AuthNav />}
         </Header>
         <Routes>
           <Route path="/" element={<NavBar />}>
             <Route path="home" element={<Home />} />
           </Route>
           <Route index element={<Navigate to={"home"} />}></Route>
+          <Route
+            path="/register"
+            element={
+              <RestrictedRoute
+                redirectTo="/home"
+                component={<RegisterPage />}
+              />
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <RestrictedRoute redirectTo="/home" component={<LoginPage />} />
+            }
+          />
+          <Route
+            path="/home"
+            element={<PrivateRoute redirectTo="/login" component={<Home />} />}
+          />
+
           <Route path="region" element={<RegionList />} />
           <Route path="region/:locPath" element={<FishingLocationsList />} />
           <Route path="region/:locPath/:_id" element={<PreciseLocation />} />
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,21 +1,28 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { Outlet } from "react-router-dom";
 import css from "./NavBar.module.css";
+import { useAuth } from "../../Utilities/Hooks/useAuth";
 
 const NavBar = () => {
   const navItem = [
-    { href: "home", text: "Home" },
     { href: "newData", text: "Add your own data" },
     { href: "region", text: "Fishing locations" },
     { href: "fishes", text: "Fish" },
   ];
+  const { isLoggedIn } = useAuth();
+
+
 
   return (
     <nav className={css.navBar}>
-      {navItem.map(({ href, text }) => (
-        <Link to={href} key={href} className={css.link}>
+      <NavLink className={css.link} to="/">
+        <span className={css.text}> Home</span>
+        
+      </NavLink>
+      {isLoggedIn && navItem.map(({ href, text }) => (
+        <NavLink to={href} key={href} className={css.link}>
           <span className={css.text}> {text}</span>
-        </Link>
+        </NavLink>
       ))}
       <Outlet />
     </nav>
